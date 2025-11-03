85f58be568aeb814df4de65c85bbe6f0a3b3adaa
06-Pongpan <Pongpan.sri@sbac.ac.th>
2025-11-03 09:56:31 +0700

    first commit

---
+++ bin/dart_practics_2.dart
@@ -0,0 +1,5 @@
+import 'package:dart_practics_2/dart_practics_2.dart' as dart_practics_2;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_practics_2.calculate()}!');
+}
---
+++ test/dart_practics_2_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_practics_2/dart_practics_2.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
